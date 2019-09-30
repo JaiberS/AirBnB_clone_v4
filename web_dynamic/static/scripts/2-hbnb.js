224d511cfabe691812d4bd5626ed4a82aa069dfa
JaiberS <758@holbertonschool.com>
2019-09-30 23:25:32 +0000

    [Feature] Uploading for the third task, creating API status div

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
   const lista = [];
+  const nombres = [];
   $('input[type="checkbox"]').click(function () {
     if ($(this).prop('checked') === true) {
       lista.push($(this).attr('data-id'));
+      nombres.push($(this).attr('data-name'));
     } else if ($(this).prop('checked') === false) {
       lista.splice($.inArray($(this).attr('data-id'), lista), 1);
-    }
+      nombres.splice($.inArray($(this).attr('data-name'),nombres), 1);
+      }
+    console.log(nombres);
+    $(".amenities h4").text(nombres);
   });
   let jqxhr = $.get( "http://0.0.0.0:5001/api/v1/status/", function() {
     alert( "success" );
-    $("api_status").addClass("available");
+    $("DIV#api_status").addClass("available");
   })
   .done(function() {
     alert( "second success" );
   })
   .fail(function() {
     alert( "error" );
-    $("api_status").removeClass("available");
+    $("DIV#api_status").removeClass("available");
   })
   .always(function() {
     alert( "finished" );
