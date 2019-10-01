ee5ef459c29baf12a148128ef33f0e4a9cc5d0bb
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-10-01 03:11:59 +0000

    [Feature] Uploading the 4th task

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,17 +13,9 @@ $(document).ready(function () {
     $(".amenities h4").text(nombres);
   });
   let jqxhr = $.get( "http://0.0.0.0:5001/api/v1/status/", function() {
-    alert( "success" );
     $("DIV#api_status").addClass("available");
   })
-  .done(function() {
-    alert( "second success" );
-  })
   .fail(function() {
-    alert( "error" );
     $("DIV#api_status").removeClass("available");
   })
-  .always(function() {
-    alert( "finished" );
-  });
 });
