218d6f29345301cfab5f0b57522a526c081b2fec
JaiberS <758@holbertonschool.com>
2019-12-04 23:36:33 +0000

    [Feature] Changed models to met only users and users tasks

--- models/review.py
+++ models/review.py
@@ -12,10 +12,10 @@ class Review(BaseModel, Base):
     """Review class handles all application reviews"""
     if STORAGE_TYPE == "db":
         __tablename__ = 'reviews'
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        description = Column(String(1024), nullable=False)
+        state = Column(String(10), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     else:
-        place_id = ''
+        state = ''
         user_id = ''
-        text = ''
+        description = ''
