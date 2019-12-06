d4cd3a3b45518bea6ce802660a7d81957ac77702
JaiberS <758@holbertonschool.com>
2019-12-06 11:48:08 +0000

    [Feature] Button to Create a Task

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -76,6 +76,8 @@ $(".close").click(function() {
       });
     $("#myModal").show();
     $("#b6").show();
+     $("#b7").hide();
+     $("#b8").hide();
  });
 
 
@@ -123,13 +125,15 @@ $(".close").click(function() {
       });
     $("#myModal").show();
     $("#b7").show();
+     $("#b8").hide();
+     $("#b6").hide();
  });
 
 
   $('#b7').click(function () {
-      $(".places article").remove();
       $(".locations h4").text('');
       for (iter in listb){
+      $(".places article").remove();
       if ($('#0rad' + listb[iter]).is(':checked')){
         dictask = {"description": document.getElementById("text" + listb[iter]).value, "state": "to_do"} 
       } else {
@@ -138,6 +142,66 @@ $(".close").click(function() {
       $.ajax({
               type : "PUT",
               url : "http://localhost:5003/api/v1/places/" + listb[iter],
+              contentType: "application/json",
+              data : JSON.stringify(dictask),
+              success : function(data) {
+            },
+      });
+      } 
+    $.ajax({
+          type : "GET",
+          url : url,
+          contentType : "application/json",
+          success : function(data) {
+             $.each(data, function(key, value) {
+                 
+                 $('.places').append('<article><div class="title"><h2>' + value.name + '</h2></div><div class="information"></div><div class="description' + value.id + '"></div></article>'); 
+                 for (iter in value.tasks){
+                    $('.places .description' + value.id).append('<h1>' + value.tasks[iter].description + '</h1><h2>' + value.tasks[iter].state +'</h2>')
+                 }
+              });
+            },
+          error : function(error) {
+          },
+        });
+    listb = []
+     $("#b7").hide();
+    $("#myModal").hide();
+ });
+
+ $("#b4").click(function() {
+     $(".modal-content .tform").remove();
+      $.ajax({
+              type : "POST",
+              url : "http://localhost:5003/api/v1/places_search/",
+              contentType: "application/json",
+              data : JSON.stringify({users: lista}),
+              success : function(data) {
+                $.each(data, function(key, value) {        
+                      $(".modal-content").append('<div class="tform"></div>')
+                      $(".modal-content .tform").append('Description<br><input type="text" name="description" value="description" id="createdesc"><br>User_id<br><input type="text" name="user_id" value=' + value.id + ' id="createuserid"><br><p><input type="radio" name="state" value=state id="0radcreate" required checked>to_do<br><input type="radio" name="state" value=state id="1radcreate" required>done</p><br>')
+                });
+            },
+      });
+    $("#myModal").show();
+    $("#b8").show();
+     $("#b7").hide();
+     $("#b6").hide();
+ });
+
+
+  $('#b8').click(function () {
+      $(".places article").remove();
+      $(".locations h4").text('');
+      if ($('#0radcreate').is(':checked')){
+        dictask = {"description": document.getElementById("createdesc").value, "state": "to_do", "user_id":  document.getElementById("createuserid").value} 
+      } else {
+        dictask = {"description": document.getElementById("createdesc").value, "state": "done", "user_id":  document.getElementById("createuserid").value} 
+      }
+      console.log(dictask)
+      $.ajax({
+              type : "POST",
+              url : "http://localhost:5003/api/v1/places/123",
               contentType: "application/json",
               data : JSON.stringify(dictask),
               success : function(data) {
@@ -150,11 +214,12 @@ $(".close").click(function() {
                 });
             },
       });
-      } 
     listb = []
-     $("#b7").hide();
+     $("#b8").hide();
     $("#myModal").hide();
  });
+$
+
 $("#b2").click(function() {
       $(".places article").remove();
       $(".locations h4").text('');
